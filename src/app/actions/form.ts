4a29b21530939acf005ff82a54d3de2bf7d02eb0
venkatsundaraj <venkateshsundarasan@gmail.com>
2025-08-28 18:17:46 +0530

    form

--- src/app/actions/form.ts
+++ src/app/actions/form.ts
@@ -1,7 +1,14 @@
 "use server";
 
 import { DataSchema } from "@/app/(convex)/controller-rhf/page";
+import { openai } from "@ai-sdk/openai";
 
 export async function submitData(data: DataSchema) {
   console.log(data);
 }
+
+// export async function generateImage(prompt:string):Promise<string>{
+//   try{
+//     const response = await openai.images
+//   }
+// }
---
+++ src/app/generate-image/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { ChangeEvent, FC, FormEvent, useState } from "react";
+import { useMutation, useQuery } from "convex/react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const submitHandler = async function (e: FormEvent) {
+    e.preventDefault();
+    console.log(inputValue);
+    setInputValue("");
+  };
+
+  const changeHandler = function (e: ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  };
+  return (
+    <section className="w-full flex flex-col items-center justify-center min-h-screen">
+      <form
+        onSubmit={submitHandler}
+        className="min-w-xl p-4 flex items-center justify-center flex-col gap-3"
+      >
+        <input
+          value={inputValue}
+          onChange={changeHandler}
+          type="text"
+          className="w-full border-white border"
+        />
+        <button
+          className="bg-green-500 cursor-pointer px-4 py-2 rounded-md"
+          type="submit"
+        >
+          submit
+        </button>
+      </form>
+      <ul className="flex flex-col items-start justify-center gap-2">
+        {/* {getTodos?.map((item, i) => (
+          <li className="text-white" key={i}>
+            {item.text}
+          </li>
+        ))} */}
+      </ul>
+    </section>
+  );
+};
+
+export default page;
